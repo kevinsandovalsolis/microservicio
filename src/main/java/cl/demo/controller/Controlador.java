5c8801add3998e48b739ee16e67781d48e68f5d7
Adinlm <adin.lopez.mun@gmail.com>
2021-05-13 16:06:03 -0400

    practicas con @GetMapping

--- src/main/java/cl/demo/controller/Controlador.java
+++ src/main/java/cl/demo/controller/Controlador.java
@@ -2,19 +2,51 @@ package cl.demo.controller;
 
 import cl.demo.dto.ResponseDto;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class Controlador {
-
-
 
 
     @GetMapping(value = "/demoget", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseDto getClienteFactoring() {
         return new ResponseDto("prueba");
     }
+
+    @GetMapping("/ejemplo")
+    public String getEjemplo(@RequestParam(required = false) String id, @RequestParam String name, @RequestParam String lastname) {
+        return "ID: " + id + " nombre: " + name + " apellido: " + lastname;
+    }
+
+    @GetMapping("/ejemploptional")
+    public String getEjemploptional(@RequestParam Optional<String> id) {
+        return "ID: " + id.orElseGet(() -> "No provisto");
+    }
+
+    @GetMapping("/default")
+    public String getDefault(@RequestParam(defaultValue = "prueba") String name) {
+        return "nombre: " + name;
+    }
+
+    @PostMapping("/map")
+    public String updateMap(@RequestParam Map<String,String> allParams) {
+        return "Los parametros son: " + allParams.entrySet();
+    }
+
+    @PostMapping("/mapparam")
+    public String updateMapparam(@RequestParam Map<String,String> allParams) {
+        return "Parameters are " + allParams.entrySet();
+    }
+
+    @GetMapping("/multivalue")
+    public String getMultivalue(@RequestParam List<String> names) {
+        return "Nombres son " + names;
+    }
+
 }
+
+
